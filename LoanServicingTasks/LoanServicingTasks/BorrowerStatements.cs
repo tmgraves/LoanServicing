fb48df7850cabc083453b0c961265f8562807629
tmgraves <tadashigraves@gmail.com>
2015-07-22 13:52:57 -0700

    add statement tasks

---
+++ LoanServicingTasks/LoanServicingTasks/BorrowerStatements.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LoanServicingTasks.utiloan;
+
+namespace LoanServicingTasks
+{
+    class BorrowerStatements
+    {
+        static void Main(string[] args)
+        {
+            Tasks webService = new Tasks();
+            bool result = webService.ProcessBorrowerStatements("tadashigraves@gmail.com", "Yuki13", DateTime.Today);
+            //  bool result = webService.ProcessBorrowerStatements("tadashigraves@gmail.com", "Yuki13", Convert.ToDateTime("6/30/2013"));
+        }
+    }
+}
---
+++ RunInvestorStatements/RunInvestorStatements/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RunInvestorStatements.utiloan;
+
+namespace RunInvestorStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tasks webService = new Tasks();
+            bool result = webService.ProcessInvestorStatements("tadashigraves@gmail.com", "Yuki13", DateTime.Today);
+            //  bool result = webService.ProcessInvestorStatements("tadashigraves@gmail.com", "Yuki13", Convert.ToDateTime("6/30/2013"));
+        }
+    }
+}
